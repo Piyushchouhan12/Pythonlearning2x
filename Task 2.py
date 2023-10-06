b3096f71ff1457584340f1a4623c2663f7b35a8b
Piyushchouhan12 <65841068+Piyushchouhan12@users.noreply.github.com>
2023-10-06 13:35:23 +0530

    Add files via upload

---
+++ Task 2.py
@@ -0,0 +1,14 @@
+#Print the Name of the user by taking it from the input command.
+
+Name = input("Enter your name")
+print(Name)
+#Take first name and last name of user and print with sep. = "-" and end with /t
+firstname =input("enter you first name")
+Secondname =input("Enter your second name")
+print(firstname , Secondname, sep="-", end='\t')
+
+#Take a user input as name and say with message
+#You are welcome. "<Pramod>" to the Python Class.
+
+Username= input("Enter username")
+print('You are welcome', Username ,'to the Python class' )
